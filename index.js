3f7169ad6d433b66992f0e639d36862feeda06df
Matt Beaudoin <ehxtwo@gmail.com>
2021-06-07 09:01:48 -0700

    commit tree 2bfa9423889243be64de84d2bc73d90f4223a77b

--- index.js
+++ index.js
@@ -9,5 +9,22 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+
 /* Write your implementation of greet() */
+
+function greet(givenTime) {
+  if (parseInt(givenTime.split(':'))<12)
+  return "Good Morning"
+  if (parseInt(givenTime.split(':'))<17)
+  return "Good Afternoon"
+  if (parseInt(givenTime.split(':'))>16)
+  return "Good Evening"
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  const greeting = string
+  greeting
+
+}
